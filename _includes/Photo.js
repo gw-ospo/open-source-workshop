86d1002e9be13e958daabd5d30b289341654c26f
Max Turer <max.turer@gwu.edu>
2025-03-19 15:28:32 -0400

    photos grabbed from folder and animation set up

---
+++ _includes/Photo.js
@@ -0,0 +1,28 @@
+class Photo {
+  constructor(img, x, y){
+    this.x = x;
+    this.y = y;
+    this.dx = 2;
+    this.dy = 3;
+    this.img = img;
+  }
+  
+  drawImg() {
+    this.img.resize(width/4, 0);
+    image(this.img, this.x, this.y);
+  }
+  
+  moveImg() {
+    this.x = this.x + this.dx;
+    this.y = this.y + this.dy;
+  }
+  
+  checkBoundary() {
+    if (this.x > width || this.x < 0){
+      this.dx = this.dx * -1;
+    }
+    if (this.y > height || this.y < 0){
+      this.dy = this.dy * -1;
+    }
+  }
+}
